e55edf00590e6560b93d745ccea9bbe5b0b2d8cb
aeife <andre.eife@gmail.com>
2014-02-23 17:50:01 +0100

    added knightgame config and test

--- src/knightGame/knightGame.js
+++ src/knightGame/knightGame.js
@@ -5,9 +5,20 @@
 angular.module('knightGame', ['knightGame.knight'])
 
     .factory('knightGame', function($log) {
+        var config = {
+            knightCount: 10,
+            strategy: null
+        };
+
         return {
-            start: function(){
+            start: function () {
                 $log.info("starting the simulation...");
+            },
+            config: function (newConfig) {
+               angular.extend(config, newConfig);
+            },
+            getConfig: function () {
+                return config;
             }
-        }
+    }
     });
---
+++ src/knightGame/knightGame.spec.js
@@ -0,0 +1,24 @@
+/**
+ * Created by aeife on 23/02/14.
+ */
+
+describe("knightGame", function () {
+    var knightGame;
+
+    beforeEach(module('knightGame'));
+    beforeEach(inject( function (_knightGame_) {
+        knightGame = _knightGame_;
+    }));
+
+    it("should have a start function", function () {
+        expect(knightGame.start).toBeDefined();
+    });
+
+    it("should allow change of configs", function () {
+        knightGame.config({
+            knightCount: 99
+        });
+
+        expect(knightGame.getConfig().knightCount).toBe(99);
+    });
+});
